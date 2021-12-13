f6966db0e28b0f312565bb66312a02cba485f6e8
Michael Klug <michiklug85@gmail.com>
2021-12-13 22:48:10 +0100

    day 9 part 1

--- day9/main.go
+++ day9/main.go
@@ -4,25 +4,55 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"strconv"
 	"strings"
 )
 
 func main() {
-	bytes, err := ioutil.ReadFile("day9/input-small.txt")
+	bytes, err := ioutil.ReadFile("day9/input.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
 	text := string(bytes)
 	textSlice := strings.Split(text, "\n")
 
-	fmt.Println(textSlice)
-
 	c := caves{}
 	c = make([][]int, len(textSlice)+2)
-	for _, line := range c {
-		line = make([]int, len(line)+2)
+	for i := range c {
+		c[i] = make([]int, len(textSlice[0])+2)
 	}
 
+	numbers := make([][]int, len(textSlice))
+	for i := range numbers {
+		numbers[i] = make([]int, len(textSlice[0]))
+	}
+	for i, line := range textSlice {
+		for j, val := range line {
+			num, _ := strconv.Atoi(string(val))
+			numbers[i][j] = num
+		}
+	}
+
+	for i := range c {
+		for j := range c[i] {
+			if i == 0 || i == len(c)-1 || j == 0 || j == len(c[0])-1 {
+				c[i][j] = 9
+			} else {
+				c[i][j] = numbers[i-1][j-1]
+			}
+		}
+	}
+
+	lowPoints := 0
+	for i, line := range c[1 : len(c)-1] {
+		for j, val := range line[1 : len(line)-1] {
+			if val < c[i][j+1] && val < c[i+1][j] && val < c[i+2][j+1] && val < c[i+1][j+2] {
+				lowPoints += val + 1
+			}
+		}
+	}
+
+	fmt.Println("answer part 1: ", lowPoints)
 }
 
 type caves [][]int
